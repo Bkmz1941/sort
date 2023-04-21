17f28a89d5768906a7a291ffa23c6fd13dc8eabb
Ilya Titov <bkmz1941@rambler.ru>
2023-04-21 10:32:53 +0300

    project init

---
+++ src/count/CountSort.java
@@ -0,0 +1,10 @@
+package count;
+
+public class CountSort {
+
+
+    public static void main(String[] args) {
+        int[] array = new int[] {64, 42, 73, 41, 32, 53, 16, 24, 57, 42, 74, 55, 36};
+
+    }
+}
